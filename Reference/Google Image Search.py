c6edeb5eaddf3a878acef2e8bb9d90db345cfa1d
Libby <pluet_@naver.com>
2019-06-04 00:42:19 +0900

    todo list

---
+++ Reference/Google Image Search.py
@@ -0,0 +1,3 @@
+# Google Custom Search : Non-profit Search Element
+# https://developers.google.com/custom-search/docs/overview
+
---
+++ bookshelf_opencv_trial.py
@@ -0,0 +1,11 @@
+# Trial points (image)
+# - image resolution
+# - image size
+# - bookshelf shape(rounded, tilted, irregular, etc.)
+# - bookshelf with books and other stuffs
+# - include partial image of books
+# - ratio : bookshelf - background
+
+import cv2 as cv
+import numpy as np
+
