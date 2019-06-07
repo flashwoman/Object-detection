5386d8ec23ec19ebe2ce8d971affabed3ec498cd
Libby <pluet_@naver.com>
2019-06-07 17:52:27 +0900

    ongoing....20190607

--- test_ovrl_bookshelf_opencv.py
+++ test_ovrl_bookshelf_opencv.py
@@ -8,15 +8,13 @@
 
 import cv2 as cv
 import numpy as np
-import imutils
 import matplotlib.image as mpimg
 from PIL import Image
-import time
 
 
 # 받아온 사진 사이즈 고정시키기 (1100, 600)
 size = (900, 600)
-path = "C:/Users/DELL/PycharmProjects/Object-detection/image/bookshelf_04.jpg"
+path = "C:/Users/DELL/PycharmProjects/Object-detection/image/books.jpg"
 img = Image.open(path)
 resized_image = img.resize(size)
 
@@ -101,64 +99,6 @@ cv.destroyAllWindows()
 cropImage = im.crop((100, 100, 150, 150)) # crop()의 파라미터는 (좌, 상, 우, 하) 위치를 갖는 튜플로 지정한다.
 cropImage.save('python-crop.jpg')
 '''
-
-
-# Sobel 함수와 Harris Corner Detection 사용해서 thresh4(책장) 코너점 검출하기 (구현코드 --- 더 미세하게 조정할 수 있음)
-
-img_sobel_x = cv.Sobel(thresh4, cv.CV_32F, 1, 0)
-img_sobel_y = cv.Sobel(thresh4, cv.CV_32F, 0, 1)
-
-
-IxIx = img_sobel_x * img_sobel_x
-IyIy = img_sobel_y * img_sobel_y
-IxIy = img_sobel_x * img_sobel_y
-
-
-height, width = img.shape[:2]
-
-window_size = 5
-offset = int(window_size/2)
-
-r = np.zeros(thresh4.shape)
-
-start = time.process_time()
-for y in range(offset, height-offset):
-    for x in range(offset, width-offset):
-        window_IxIx = IxIx[y-offset:y+offset+1, x-offset:x+offset+1]
-        window_IyIy = IyIy[y-offset:y+offset+1, x-offset:x+offset+1]
-        window_IxIy = IxIy[y-offset:y+offset+1, x-offset:x+offset+1]
-
-        Mxx = window_IxIx.sum()
-        Myy = window_IyIy.sum()
-        Mxy = window_IxIy.sum()
-
-
-        det = Mxx*Myy - Mxy*Mxy
-        trace = Mxx + Myy
-
-        r[y,x] = det - 0.04 * (trace ** 2)
-
-
-cv.normalize(r,r,0.0,1.0,cv.NORM_MINMAX)
-
-for y in range(offset, height-offset):
-    for x in range(offset, width-offset):
-        if r[y, x] > 0.4:
-            img.itemset((y, x, 0), 0)
-            img.itemset((y, x, 1), 0)
-            img.itemset((y, x, 2), 255)
-
-
-end = time.process_time()
-print(end-start)
-
-cv.imshow("original", img)
-cv.waitKey(0)
-
-# images_combiend 파일 저장하기
-path = "C:/Users/DELL/PycharmProjects/Object-detection/image/images_thresh4_Point.jpg"
-cv.imwrite(path, img)
-
 
 
 
--- test_shapeDetector.py
+++ test_shapeDetector.py
@@ -1,91 +1,7 @@
-# 모양을 구분해주는 ShapeDetector
+# Reference에 들어있는 OpneCV_ShapeDetect Code가 실행되지 않아 찾아본 다른 코드.
+# Reference Code <https://webnautes.tistory.com/1296>
 import cv2 as cv
 
-
-# # 전처리한 사진 다른이름으로 저장하기
-# path = "C:/Users/DELL/PycharmProjects/Object-detection/image/test_shelf_resized.jpg"
-# # resized_image.save(path)
-# img = cv.imread(path, cv.IMREAD_COLOR)
-# img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # shape : (600, 900)
-#
-#
-# # cv2의 Corner Harris 함수 사용 (전처리 후)
-# # 책장 검출 용이한 사진으로 전처리
-# # ret,thresh1 = cv.threshold(img_gray,127,255,cv.THRESH_BINARY)
-# ret,thresh2 = cv.threshold(img_gray,127,255,cv.THRESH_BINARY_INV) # 책 아웃라인 검출
-#
-#
-# class ShapeDetector:
-#
-#     def __init__(self):
-#
-#         pass
-#
-#     def detect(self, c):
-#
-#         # initialize the shape name and approximate the contour
-#         shape = "unidentified"
-#         peri = cv.arcLength(c, True)
-#         approx = cv.approxPolyDP(c, 0.04 * peri, True)
-#
-#
-#         # if the shape is a triangle, it will have 3 vertices
-#         if len(approx) == 3:
-#
-#             shape = "triangle"
-#
-#
-#         # if the shape has 4 vertices, it is either a square or a rectangle
-#         elif len(approx) == 4:
-#
-#             # compute the bounding box of the contour and use the
-#             # bounding box to compute the aspect ratio
-#             (x, y, w, h) = cv.boundingRect(approx)
-#             ar = w / float(h)
-#
-#             # a square will have an aspect ratio that is approximately equal to one, otherwise, the shape is a rectangle
-#             shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
-#
-#
-#         # if the shape is a pentagon, it will have 5 vertices
-#         elif len(approx) == 5:
-#
-#             shape = "pentagon"
-#
-#
-#         # otherwise, we assume the shape is a circle
-#         else:
-#
-#             shape = "circle"
-#
-#
-#         # return the name of the shape
-#         return shape
-#
-#
-# # cv2.findContours 함수는 윤곽이 있는 부분을 찾아주는 함수이다.
-#
-# sd = ShapeDetector()
-# cnts = cv.findContours(thresh2.copy(), cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-# cnts = cnts[0] if imutils.is_cv2() else cnts[1] # 찾은 contour들 cnts에 저장된다.
-#
-# # 찾아진 contour들 사진에 표시해서 띄우기
-# for c in cnts:
-#
-#     if sd.detect(c) != 'rectangle': next
-#     c = c.astype("float")
-#     c = c.astype("int")
-#     x, y, w, h = cv.boundingRect(c)
-#     cv.rectangle(thresh2, (x, y), (x + w, y + h), (3, 255, 4), 2)
-#     cv.imshow("image", img)
-#     cv.waitKey(0)
-#
-# cv.destroyAllWindows()
-
-
-
-
-# 위 코드가 되지 않아서 찾아본 Reference Code <https://webnautes.tistory.com/1296>
 
 def setLabel(image, str, contour):
     (text_width, text_height), baseline = cv.getTextSize(str, cv.FONT_HERSHEY_SIMPLEX, 0.7, 1)
@@ -112,42 +28,54 @@ contours, hierarchy = cv.findContours(img_binary, cv.RETR_EXTERNAL, cv.CHAIN_APP
 # RETR_EXTERNAL : 외곽의 컨투어만 검출하도록 한다. / CHAIN_APPROX_SIMPLE : 검출되는 컨투어의 구성점 개수 줄이기(ex. 직선부분 양끝점만 검출하기)
 
 
+
 # 검출된 컨투어를 직선으로 근사화시키기
 for cnt in contours:
 
     # 컨투어 구성성분 개수 확인하기
-    size = len(cnt)
-    print(size)
+    size1 = len(cnt)
+    # print(size1)
 
     # 컨투어를 직선으로 근사화시키기 2
-    epsilon = 0.005 * cv.arcLength(cnt, True)
+    epsilon = 0.005 * cv.arcLength(cnt, True) # 몇%나 직선으로 근사화시킬 것인지 결정 (현재는 0.5%)
     approx = cv.approxPolyDP(cnt, epsilon, True)
 
     # 직선으로 근사화시킨 컨투어의 구성성분 개수 확인하기
-    size = len(approx)
-    print(size)
-#
-#     cv.line(img_color, tuple(approx[0][0]), tuple(approx[size-1][0]), (0, 255, 0), 3)
-#     for k in range(size-1):
-#         cv.line(img_color, tuple(approx[k][0]), tuple(approx[k+1][0]), (0, 255, 0), 3)
-#
-#     if cv.isContourConvex(approx):
-#         if size == 3:
-#             setLabel(img_color, "triangle", cnt)
-#         elif size == 4:
-#             setLabel(img_color, "rectangle", cnt)
-#         elif size == 5:
-#             setLabel(img_color, "pentagon", cnt)
-#         elif size == 6:
-#             setLabel(img_color, "hexagon", cnt)
-#         elif size == 8:
-#             setLabel(img_color, "octagon", cnt)
-#         elif size == 10:
-#             setLabel(img_color, "decagon", cnt)
-#         else:
-#             setLabel(img_color, str(size), cnt)
-#     else:
-#         setLabel(img_color, str(size), cnt)
-#
-# cv.imshow('result', img_color)
-# cv.waitKey(0)
+    size2 = len(approx)
+    # print(size2-size1) # 대부분 0 or (-) 값이면 직선 근사화가 잘 된 것으로 파악한다.
+
+    # 직선으로 근사화된 컨투어를 사진에 표시하기
+    cv.line(img, tuple(approx[0][0]), tuple(approx[size2-1][0]), (0, 255, 0), 3) # 녹색선으로 표시
+    for k in range(size2-1):
+        cv.line(img, tuple(approx[k][0]), tuple(approx[k+1][0]), (0, 255, 0), 3) # 녹색선으로 표시
+
+    # cv.imshow('result', img)
+    # cv.waitKey(0)
+
+    # 컨투어 내부에 도형 이름 표시하기
+    if cv.isContourConvex(approx): # isContourConvex함수를 통해 오목하게 들어간 도형을 검출제외시킨다.
+        if size2 == 3:
+            setLabel(img, "triangle", cnt)
+        elif size2 == 4:
+            setLabel(img, "rectangle", cnt)
+        elif size2 == 5:
+            setLabel(img, "pentagon", cnt)
+        elif size2 == 6:
+            setLabel(img, "hexagon", cnt)
+        elif size2 == 8:
+            setLabel(img, "octagon", cnt)
+        elif size2 == 10:
+            setLabel(img, "decagon", cnt)
+        else:
+            setLabel(img, str(size2), cnt)
+    else:
+        setLabel(img, str(size2), cnt)
+
+
+# images_combiend 파일 저장하기
+path = "C:/Users/DELL/PycharmProjects/Object-detection/image/images_shapeDetected.jpg"
+cv.imwrite(path, img)
+
+cv.imshow('result', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
